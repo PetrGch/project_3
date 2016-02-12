9d02d7882622eb7e46a1e97fe9c0b6213697c3f6
Petr <Petr.Gulchuk@gmail.com>
2016-02-12 20:15:40 +0500

    first commit

--- jq/myJs.js
+++ jq/myJs.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-
-	var widthScreen = screen.width;
-
-	$(".header h1").click(function() {
-		$('.slider img:eq(0)').animate({
-			marginLeft: "0px",
-			width: widthScreen,
-		}, 1000);
+	
+	function toggle (value) {
+			if (value == 1) {
+				$('.ourStory, .work, .journal, .contact').slideUp(2000);
+				$('.services').slideToggle(500);
+			} else if (value == 2) {
+				$('.services, .work, .journal, .contact').slideUp(500);
+				$('.ourStory').slideToggle(500);
+			} else if (value == 3) {
+				$('.ourStory, .services, .journal, .contact').slideUp(500);
+				$('.work').slideToggle(500);
+			} else if (value == 4) {
+				$('.ourStory, .work, .services, .contact').slideUp(500);
+				$('.journal').slideToggle(500);
+			} else if (value == 5) {
+				$('.ourStory, .work, .journal, .services').slideUp(500);
+				$('.contact').slideToggle(500);
+			};
+		}
 		
-
-	})
+$(document).ready(function() {
 
 });
